1f8a62d185793bc69f89aba81bdec23aea0aaf79
fernandabonetti <fernandasbonetti@gmail.com>
2019-07-09 16:48:27 -0300

    Dumping switch statistics to JSON

--- receiver.py
+++ receiver.py
@@ -1,18 +1,22 @@
+#!/usr/bin/env python
+
 import argparse
 import sys
 import socket
 import random
 import struct
+import json
 
 from scapy.all import sendp, send, sniff, hexdump, get_if_list, get_if_hwaddr
 from scapy.all import Ether, IP, UDP, TCP
 from scapy.all import IntField, FieldListField, FieldLenField, ShortField, PacketListField, XBitField
 from scapy.packet import Packet, bind_layers
 from intHeader import SwitchTrace
+from time import sleep
+from datetime import datetime
 
 TYPE_INT = 0x1212
 
-from time import sleep
 
 def get_if():
     ifs=get_if_list()
@@ -22,20 +26,38 @@ def get_if():
             iface=i
             break;
     if not iface:
-        print "Cannot find eth0 interface"
+        print("Cannot find eth0 interface")
         exit(1)
     return iface
 
-def showPacket(pkt):
+def showPacket(pkt, archivename):
+    stats = {}
+    path = []
+    hop_delays = []
+    timestamps = []
+    queue_lengths = []
+    flux = 0
+    hop_avg = 0
     if SwitchTrace in pkt:
-	print "oi"
-        pkt.show2()
+        for i in range(0, 6):
+            pkt.show()
+            hop_avg += float(pkt[SwitchTrace][i].hop_delay)
+            path.append(int(pkt[SwitchTrace][i].swid))
+            hop_delays.append(float(pkt[SwitchTrace][i].hop_delay))
+            timestamps.append(pkt[SwitchTrace][i].ingress)
+            queue_lengths.append(int(pkt[SwitchTrace][i].qdepth))
+            flux = pkt[IP].tos
+        stats[flux] = {"hop_avg": hop_avg, "path": path, "hop_delays": hop_delays, "timestamps": timestamps, "queue_stats": queue_lengths}
+
+        with open(archivename, 'w') as fp:
+            json.dump(stats, fp,  ensure_ascii=False, sort_keys=False)
+
 
 def main():
-
-    iface = 'h2-eth0'
+    archivename = "data/"+sys.argv[1]
+    iface = get_if()
     print("Interface: %s" % iface)
-    sniff(iface = iface, prn = lambda x: showPacket(x))
+    sniff(iface = iface, prn = lambda x: showPacket(x, archivename))
 
 if __name__ == '__main__':
     main()
